81928a9b8da5b93043b1067636d3c55da82ed52c
MackInSpace <amackey@lssu.edu>
2025-04-22 18:54:23 -0600

    added search values

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,11 +1,28 @@
 import { first151Pokemon, getFullPokedexNumber } from '../utils/index';
+import { useState } from 'react';
 
-export default function SideNav() {
+export default function SideNav(props) {
+    const { 
+        selectedPokemon, 
+        setSelectedPokemon, 
+        handleCloseMenu, 
+        showSideMenu 
+    } = props
 
+    const [searchValue, setSearchValue] = useState('')
+
+    const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+        if ((getFullPokedexNumber(eleIndex)). includes(searchValue)) { return true }
+        if (ele.toLowerCase().includes(searchValue.toLowerCase())) { return true }
+        return false
+    })
 
     return (
-        <nav>
-            <div className={'header'}>
+        <nav className={' ' + (!showSideMenu ? 'open' : '')}>
+            <div className={'header' + (!showSideMenu ? ' open' : '')}>
+                <button onClick={handleCloseMenu} className='open-nav-button'>
+                    <i className='fa-solid fa-arrow-left-long'></i>
+                </button>
                 <h1 className="text-gradient">Pokédex</h1>
             </div>
             <input />
